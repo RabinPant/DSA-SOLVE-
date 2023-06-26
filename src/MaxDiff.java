31a6e6dfe512968668a8267ee1d2d470312134c3
= <=>
2023-06-26 10:01:08 -0400

    maxDiff

---
+++ src/MaxDiff.java
@@ -0,0 +1,20 @@
+public class MaxDiff {
+    static int maxDiff(int[]arr, int arr_length){
+
+        int maxDiff = arr[1]-arr[0];
+        int min = arr[0];
+        int res = 0;
+        for(int i=1;i<arr_length;i++){
+            res = Math.max(res,arr[i]-min);
+            min = Math.min(min,arr[i]);
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+
+        int arr[] = {2,3,10,6,4,8,1};
+        int result = maxDiff(arr, arr.length);
+        System.out.println(result);
+    }
+}
